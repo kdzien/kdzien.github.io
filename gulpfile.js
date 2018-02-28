a65f8aad6198fabd568233b26f692c3944e51fb8
kdzien <dzieniu@gmail.com>
2018-02-28 13:08:33 +0100

    gulp mergecss, about timeline

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const livereload = require('gulp-livereload');  
 const browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('sass', function () {
     return gulp.src('./styles/sass/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./styles/css'))
-      .pipe(browserSync.stream());
   });
 gulp.task('scripts', ()=> {  
     return gulp.src('scripts/*.js')
@@ -16,7 +17,14 @@ gulp.task('scripts', ()=> {
 gulp.task('html',()=>{  
     return gulp.src('*.html')
     .pipe(browserSync.stream());
-});
+})
+gulp.task('mergecss',()=>{
+    return gulp.src('./styles/css/*.css')
+    .pipe(minifyCSS())
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('./styles/dist/'))
+    .pipe(browserSync.stream());
+})
 
 gulp.task('watch',()=> {  
     browserSync.init({
@@ -24,6 +32,7 @@ gulp.task('watch',()=> {
     });
     gulp.watch('./styles/sass/*.scss', ['sass']);
     gulp.watch('./scripts/*.js', ['scripts']);
+    gulp.watch('./styles/css/*.css', ['mergecss']);
     gulp.watch('*.html', ['html']);
 });
 
