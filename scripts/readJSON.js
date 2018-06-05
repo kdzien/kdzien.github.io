a84dd457118c77378b285f37502414a27f34682d
Konrad <dzieniu@gmail.com>
2018-06-05 20:55:37 +0200

    remove project

--- scripts/readJSON.js
+++ scripts/readJSON.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
                 event.preventDefault();
                 $.each(value,function(index,elem){
                     let i = new Image();
-                    i.src=elem.url;
+                    i.src=elem.img;
                     $(id).append(
                         `<div class="project-small" style="background-image: url('${elem.img}')">
                             <div class="info-overlay"><div>${elem.text}</div><a href="${elem.url}" target="_blank"><i class="material-icons">&#xE89E;</i></></div>
